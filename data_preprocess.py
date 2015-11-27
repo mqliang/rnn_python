b2d8e991022c368f78ab3e254830ab1a5e99f571
starsdeep <starsdeep on ZJU>
2015-11-27 21:37:04 +0800

    rnn_python.py data_preprocess.py okay

---
+++ data_preprocess.py
@@ -0,0 +1,71 @@
+#! /usr/bin/env python
+
+import csv
+import itertools
+import operator
+import numpy as np
+import nltk
+import sys
+import os
+import time
+from datetime import datetime
+from utils import *
+
+_VOCABULARY_SIZE = int(os.environ.get('VOCABULARY_SIZE', '8000'))
+_HIDDEN_DIM = int(os.environ.get('HIDDEN_DIM', '80'))
+_LEARNING_RATE = float(os.environ.get('LEARNING_RATE', '0.005'))
+_NEPOCH = int(os.environ.get('NEPOCH', '100'))
+_MODEL_FILE = os.environ.get('MODEL_FILE')
+
+
+vocabulary_size = _VOCABULARY_SIZE
+unknown_token = "UNKNOWN_TOKEN"
+sentence_start_token = "SENTENCE_START"
+sentence_end_token = "SENTENCE_END"
+
+# Read the data and append SENTENCE_START and SENTENCE_END tokens
+print "Reading CSV file..."
+with open('data/reddit-comments-2015-08.csv', 'rb') as f:
+    reader = csv.reader(f, skipinitialspace=True)
+    reader.next()
+    # Split full comments into sentences
+    sentences = itertools.chain(*[nltk.sent_tokenize(x[0].decode('utf-8').lower()) for x in reader])
+    # Append SENTENCE_START and SENTENCE_END
+    sentences = ["%s %s %s" % (sentence_start_token, x, sentence_end_token) for x in sentences]
+print "Parsed %d sentences." % (len(sentences))
+    
+# Tokenize the sentences into words
+tokenized_sentences = [nltk.word_tokenize(sent) for sent in sentences]
+
+# Count the word frequencies
+word_freq = nltk.FreqDist(itertools.chain(*tokenized_sentences))
+print "Found %d unique words tokens." % len(word_freq.items())
+
+# Get the most common words and build index_to_word and word_to_index vectors
+vocab = word_freq.most_common(vocabulary_size-1)
+index_to_word = [x[0] for x in vocab]
+index_to_word.append(unknown_token)
+word_to_index = dict([(w,i) for i,w in enumerate(index_to_word)])
+
+print "Using vocabulary size %d." % vocabulary_size
+print "The least frequent word in our vocabulary is '%s' and appeared %d times." % (vocab[-1][0], vocab[-1][1])
+
+# Replace all words not in our vocabulary with the unknown token
+for i, sent in enumerate(tokenized_sentences):
+    tokenized_sentences[i] = [w if w in word_to_index else unknown_token for w in sent]
+
+# Create the training data
+# X_train is an array of list
+X_train = np.asarray([[word_to_index[w] for w in sent[:-1]] for sent in tokenized_sentences])
+y_train = np.asarray([[word_to_index[w] for w in sent[1:]] for sent in tokenized_sentences])
+
+
+# Save to Dist 
+np.save('data/index_to_word', index_to_word)
+np.save('data/word_to_index', word_to_index)
+np.save('data/X_train', X_train)
+np.save('data/y_train', y_train)
+
+
+
+
